c0aef08cf709774724d72f474da982818d902606
Jo Mendes <jlmendes1996@gmail.com>
2021-03-13 10:02:05 -0500

    SPA

--- src/components/CampsiteinfoComponent.js
+++ src/components/CampsiteinfoComponent.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
-class CampsiteInfo extends Component {
 
-  renderCampsite(campsite) {
+
+ function RenderCampsite({campsite}) {
     return (
       <div className="col-md-5 m-1">
-        <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-      </div>
+          <Card>
+                      <CardImg top src={campsite.image} alt={campsite.name} />
+                      <CardBody>
+                          <CardTitle>{campsite.name}</CardTitle>
+                          <CardText>{campsite.description}</CardText>
+                      </CardBody>
+                  </Card>
+        </div>
     )
   }
 
-  renderComments(comments) {
+ function RenderComments({comments}) {
     if(comments) {
       return (
         <div className="col-md-5 m-1">
@@ -41,20 +41,21 @@ class CampsiteInfo extends Component {
 
   }
 
-  render() {
-    if (this.props.campsite) {
+  function CampsiteInfo(props) {
+    if (props.campsite) {
         return (
-            <div className="container">
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div>
-            </div>
+          <div className="container">
+          <div className="row">
+              <RenderCampsite campsite={props.campsite} />
+              <RenderComments comments={props.campsite.comments} />
+          </div>
+      </div>
         );
     }
     return <div />;
 }
-}
+
+
 
 
 export default CampsiteInfo;
